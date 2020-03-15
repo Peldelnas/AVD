2c492135a65407d106869c4e15f5d4ef8dcdc7fe
Peldelnas <peldelnas@hotmail.com>
2020-03-16 00:47:07 +0100

    turret wip

---
+++ AVD/Assets/Alejandro Granell/3D/Script/TurretController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    public Transform target, aim, top;
+    public float reloadTime = 1f, turnSpeed = 5f, firePauseTime = 0.25f, range = 3;
+    public Transform[] cañonesPos;
+    public GameObject[] Lights;
+
+    private float nextFireTime, nextMoveTime;
+    public int randomCañon;
+    private Animator animT;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
