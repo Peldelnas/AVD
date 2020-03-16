b8646a511987f347bd0bbd112d11d4838ee8dfc3
Peldelnas <peldelnas@hotmail.com>
2020-03-16 04:37:53 +0100

    toqueteo unity vfx graph

--- AVD/Assets/Alejandro Granell/3D/Script/TurretController.cs
+++ AVD/Assets/Alejandro Granell/3D/Script/TurretController.cs
@@ -33,26 +33,28 @@ public class TurretController : MonoBehaviour
 
     void AimFire()
     {
-        if (target)
+        if (target != null)
         {
-            if(Time.time >= nextMoveTime)
-            {
-                aim.LookAt(target);
-                aim.eulerAngles = new Vector3(0, aim.eulerAngles.y, 0);
-                top.rotation = Quaternion.Lerp(top.rotation, aim.rotation, Time.deltaTime * turnSpeed);
-            }
-            if(Time.time >= nextFireTime && canSee == true)
+            
+            
+                Vector3 direction = target.position - transform.position;
+                direction.y = 0.0f;
+                Quaternion rotation = Quaternion.LookRotation(direction);
+                
+                top.rotation = Quaternion.Euler(rotation.eulerAngles.x, rotation.eulerAngles.y, rotation.eulerAngles.z);
+
+            if (Time.time >= nextFireTime && canSee == true)
             {
                 Fire();
             }
-            else
-            {
-                foreach (var light in lights)
-                {
-                    light.SetActive(false);
-                }
-                animT.SetBool("isFiring", false);
-            }
+            //else
+            //{
+            //    foreach (var light in lights)
+            //    {
+            //        light.SetActive(false);
+            //    }
+            //    animT.SetBool("isFiring", false);
+            //}
         }
         if (target == null)
         {
@@ -67,8 +69,7 @@ public class TurretController : MonoBehaviour
     {
         nextFireTime = Time.time + reloadTime;
         nextMoveTime = Time.time + firePauseTime;
-        lights[cañonDisparado].SetActive(true);
-        animT.SetBool("isFiring", true);
+        lights[cañonDisparado].SetActive(true);        
     }
 
     void Tracking()
@@ -84,12 +85,19 @@ public class TurretController : MonoBehaviour
             {
                 nextFireTime = Time.time + (reloadTime * 0.5f);
                 target = col.gameObject.transform;
+                //animT.SetBool("isFiring", true);
+                gameObject.GetComponent<Animator>().enabled = false;
             }
         }
     }
 
     void OnTriggerExit(Collider col)
     {
-        if (col.gameObject.transform == target) target = null;
+        if (col.gameObject.transform == target)
+        {
+            target = null;
+            //animT.SetBool("isFiring", false);
+            gameObject.GetComponent<Animator>().enabled = true;
+        }
     }
 }
