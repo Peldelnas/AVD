4afe66d58cf02e2d4a03f6786cd9e85305ac800c
Peldelnas <peldelnas@hotmail.com>
2020-03-20 19:47:36 +0100

    NavMesh anim
    today class

---
+++ AVD/Assets/Alejandro Granell/NavTest/SimpleGo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleGo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent agent;
+    public Transform Destination;
+    Vector3 previousPosition;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = Destination.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Destination.transform.position != previousPosition)
+        {
+            agent.destination = Destination.transform.position;
+            previousPosition = Destination.transform.position;
+        }
+    }
+}
