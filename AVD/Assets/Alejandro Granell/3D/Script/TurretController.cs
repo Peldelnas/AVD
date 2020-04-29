0990cd5845381cb10abd1c6ea073b93854767df3
Peldelnas <peldelnas@hotmail.com>
2020-04-29 10:01:37 +0200

    Script

--- AVD/Assets/Alejandro Granell/3D/Script/TurretController.cs
+++ AVD/Assets/Alejandro Granell/3D/Script/TurretController.cs
@@ -4,100 +4,57 @@ using UnityEngine;
 
 public class TurretController : MonoBehaviour
 {
-    public Transform target, aim, top;
-    public float reloadTime = 1f, turnSpeed = 5f, firePauseTime = 0.25f, range = 3;
-    public Transform[] cañonesPos;
-    public GameObject[] lights;
-    public bool canSee;
+    public float life = 3f;
+    public float frecuenciaDisparo = 0.5f;
+    public GameObject bullet;
+    public Transform[] bulletPositions;
+    public Animator turretAnim;
 
-    private float nextFireTime, nextMoveTime;
-    public int cañonDisparado;
-    private Animator animT;
-    
     // Start is called before the first frame update
     void Start()
     {
-        foreach (var light in lights)
-            {
-            light.SetActive(false);
-            }
-        animT = GetComponent<Animator>();
+        StartCoroutine(Die());
+        Shoot();
     }
 
     // Update is called once per frame
     void Update()
     {
-        AimFire();
-        Tracking();
+        
     }
 
-    void AimFire()
+    IEnumerator Die()
     {
-        if (target != null)
-        {
-            
-            
-                Vector3 direction = target.position - transform.position;
-                direction.y = 0.0f;
-                Quaternion rotation = Quaternion.LookRotation(direction);
-                
-                top.rotation = Quaternion.Euler(rotation.eulerAngles.x, rotation.eulerAngles.y, rotation.eulerAngles.z);
+        yield return new WaitForSeconds(life);
 
-            if (Time.time >= nextFireTime && canSee == true)
-            {
-                Fire();
-            }
-            //else
-            //{
-            //    foreach (var light in lights)
-            //    {
-            //        light.SetActive(false);
-            //    }
-            //    animT.SetBool("isFiring", false);
-            //}
-        }
-        if (target == null)
+        //DIE TO DO
+        yield return null;
+
+    }
+
+    public void selfDestruct()
+    {
+        //same as Die
+
+    }
+
+    public void Shoot()
+    {
+        int i = 0;
+        while(true)
         {
-            foreach (var light in lights)
-            {
-                light.SetActive(false);
-            }
+            Instantiate(bullet, bulletPositions[i].position, bulletPositions[i].rotation);
+            i++;
+            if (i >= bulletPositions.Length) i = 0;
+            WaitForShoot();
         }
     }
 
-    void Fire()
+    IEnumerator WaitForShoot()
     {
-        nextFireTime = Time.time + reloadTime;
-        nextMoveTime = Time.time + firePauseTime;
-        lights[cañonDisparado].SetActive(true);        
+        yield return new WaitForSeconds(frecuenciaDisparo);
+        yield return null;
     }
 
-    void Tracking()
-    {
-        Vector3 fwd = cañonesPos[cañonDisparado].TransformDirection(Vector3.forward);
-    }
 
-    void OnTriggerStay(Collider col)
-    {
-        if (!target)
-        {
-            if (col.CompareTag("Enemy"))
-            {
-                nextFireTime = Time.time + (reloadTime * 0.5f);
-                target = col.gameObject.transform;
-                //animT.SetBool("isFiring", true);
-                gameObject.GetComponent<Animator>().enabled = false;
-            }
-        }
-    }
-
-    void OnTriggerExit(Collider col)
-    {
-        if (col.gameObject.transform == target)
-        {
-            target = null;
-            //animT.SetBool("isFiring", false);
-            gameObject.GetComponent<Animator>().enabled = true;
-        }
-    }
 }
