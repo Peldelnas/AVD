b23ef1f164209f546c6a152dc59a135b087028e9
Peldelnas <peldelnas@hotmail.com>
2020-04-29 10:50:49 +0200

    torreta dispara

---
+++ AVD/Assets/Alejandro Granell/3D/Script/BulletController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed = 100f;
+    private Rigidbody rigidbodyy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyy = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbodyy.AddForce(transform.forward * speed);
+        Destroy(gameObject, 3.0f);
+    }
+}
