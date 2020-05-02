e9e15b4837c00aef7f7df20f83b62d241c18a110
Peldelnas <peldelnas@hotmail.com>
2020-05-02 07:48:43 +0200

    pickup portal done. turret terminada ?

--- AVD/Assets/Alejandro Granell/3D/Script/PickupController.cs
+++ AVD/Assets/Alejandro Granell/3D/Script/PickupController.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickupController : MonoBehaviour
 {
+    public GameObject pressToPickup;    
+    public GameObject turretController;
+    private bool inside;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pressToPickup = GameObject.Find("PressToPickup");
+        turretController = GameObject.Find("TurretController");
+        inside = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T) && inside)
+        {
+            pressToPickup.GetComponent<Image>().enabled = false;
+            turretController.GetComponent<TriggerInputT>().SendMessage("CanUse");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Character")
+        {
+            pressToPickup.GetComponent<Image>().enabled = true;
+            inside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Character")
+        {
+            pressToPickup.GetComponent<Image>().enabled = false;
+            inside = false;
+        }
     }
 }
--- AVD/Assets/Alejandro Granell/3D/Script/TriggerInputT.cs
+++ AVD/Assets/Alejandro Granell/3D/Script/TriggerInputT.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerInputT : MonoBehaviour
 {
-    [SerializeField]
+    public bool canUse;
     public bool TurretWorking = false;
+    public bool useTbool;
     public GameObject BallTurretPrefab;
     public Transform PlayerTransform;
+    public GameObject useT;
     // Start is called before the first frame update
     void Start()
     {
-
+        canUse = false;
+        useTbool = false;
+        useT = GameObject.Find("UseT");
     }
 
     // Update is called once per frame
@@ -20,14 +25,33 @@ public class TriggerInputT : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.T))
         {
 
-            if (!TurretWorking)
+            if (canUse && !TurretWorking)
             {
                 Instantiate(BallTurretPrefab, PlayerTransform.position, PlayerTransform.rotation, transform);
                 TurretWorking = true;
                 StartCoroutine(ResetTurret());
+                if(useTbool)
+                {
+                    useT.GetComponent<Image>().enabled = false;
+                }
             }                                   
 
         }
+    }
+
+    public void CanUse()
+    {
+        StartCoroutine(CanUseEnum());
+    }
+
+    IEnumerator CanUseEnum()
+    {
+        useT.GetComponent<Image>().enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        canUse = true;
+        useTbool = true;        
+        yield return null;
+
     }
 
     IEnumerator ResetTurret()
